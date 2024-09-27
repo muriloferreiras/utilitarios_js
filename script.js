c6dee0646205a0ff6c15f797e810a730e2d175c9
Murilo Ferreira <muriloferreira0497@gmail.com>
2024-09-27 08:13:50 -0300

    ok

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function abrir(){
+    let menu = document.getElementById('menu');
+    if (menu.style.display === 'none'){
+        menu.style.display = 'block'
+    }else{
+        menu.style.display = 'none'
+    }
+}
